89557f4ae1574db2dfe2310cda74f9bcc4893374
Purwansh Chaudhary <purwanshchaudhary@gmail.com>
2025-09-10 17:45:37 +0530

    tts.py

--- tts.py
+++ tts.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Text to Speech", layout="centered")
 # Header HTML (Made by is white, slightly moved up, slightly transparent)
 header_html = (
     "<h1 style='color: #FFB13B; text-align: center; margin-bottom: 5px;'>"
-    "TEXT TO SPEECH 🎙️"
+    "🎙️TEXT TO SPEECH🎙️"
     "</h1>"
     "<p style='text-align: center; font-size: 12px; color: rgba(255,255,255,0.75); margin-top: -10px;'>"
     "Made by <b>PURWANSH CHAUDHARY</b>"
@@ -78,6 +78,7 @@ footer_html = (
 )
 
 st.markdown(css + footer_html, unsafe_allow_html=True)
+
 
 
 
